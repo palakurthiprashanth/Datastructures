d9fc1cfd25021f7d069dda77a825e2fd0cf9448c
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-12 23:04:48 +0530

    minimum

--- src/arrays/productexceptindex.js
+++ src/arrays/productexceptindex.js
@@ -1,16 +1,8 @@
-function findProduct(arr) {
-  var temp = 1;
-  var product = [];
-  for (var i = 0; i < arr.length; i++) {
-    product[i] = temp;
-    temp = temp * arr[i];
-  }
-  temp = 1; // 1,1,3,12
-  for (var j = arr.length - 1; j > -1; j--) {
-    product[j] = product[j] * temp;
-    temp = temp * arr[j];
-  }
-  //alert(product);
+function findMinimum(arr) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  //alert(arr[0]);
+  return arr[0];
 }
-
-findProduct([1, 3, 4, 5]);
+findMinimum([9, 2, 3, 6, -1]);
