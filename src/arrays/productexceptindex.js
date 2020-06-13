4be90f0e6342afe418509430e3d09a016d6ab1d8
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-13 11:54:09 +0530

    miaxmin

--- src/arrays/productexceptindex.js
+++ src/arrays/productexceptindex.js
@@ -1,15 +1,12 @@
-function reArrange(arr) {
-  var temp;
-  var leftPos = 0;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] < 0) {
-      temp = arr[i];
-      arr[i] = arr[leftPos];
-      arr[leftPos] = temp;
-      leftPos++;
-    }
+function maxMin(arr) {
+  var res = [];
+  for (var i = 0; i < Math.floor(arr.length / 2); i++) {
+    res.push(arr.length - i);
+    res.push(arr[i]);
   }
-  // alert(arr);
+  if (arr.length % 2) {
+    res.push(Math.floor(arr.length / 2) + 1);
+  }
+  //alert(res);
 }
-
-reArrange([10, -1, 20, -4]);
+maxMin([1, 2, 3, 4, 5, 6, 7, 8, 9]);
