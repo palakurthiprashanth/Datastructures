50d888c4e639bd81d6262520ae1f017cb8c14255
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-13 08:37:11 +0530

    unique

--- src/arrays/productexceptindex.js
+++ src/arrays/productexceptindex.js
@@ -1,11 +1,17 @@
-function findMinimum(arr) {
-  var min = arr[0];
+function findFirstUnique(arr) {
+  var index2 = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
+    while (index2 < arr.length) {
+      if (i !== index2 && arr[i] === arr[index2]) {
+        break;
+      }
+      index2++;
     }
+    if (index2 === arr.length) {
+      return arr[i];
+    }
+    //alert(arr[i]);
   }
-  //alert(min);
-  return min;
 }
-findMinimum([9, 2, 3, 6, -1]);
+
+findFirstUnique([9, 2, 3, 6, 2, 6, 9, 0, 3]);
