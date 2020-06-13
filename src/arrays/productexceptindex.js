399f1d23db324a12084a0272c5dcaef3e0c6498c
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-13 09:32:57 +0530

    single loop

--- src/arrays/productexceptindex.js
+++ src/arrays/productexceptindex.js
@@ -1,16 +1,14 @@
 function findSecondMaximum(arr) {
-  var firstMax = Number.NEGATIVE_INFINITY;
-  var secondMax = Number.NEGATIVE_INFINITY;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > firstMax) {
-      firstMax = arr[i];
+  var max = Number.NEGATIVE_INFINITY;
+  var secondmax = Number.NEGATIVE_INFINITY;
+  for (var val of arr) {
+    debugger;
+    if (val > max) {
+      max = val;
     }
+    if (val > secondmax && val !== max) secondmax = val;
   }
-  for (var j = 0; j < arr.length; j++) {
-    if (arr[j] < firstMax && arr[j] > secondMax) {
-      secondMax = arr[j];
-    }
-  }
-  //alert(secondMax);
+  //alert(secondmax);
+  return secondmax;
 }
-findSecondMaximum([9, 2, 3, 6]);
+findSecondMaximum([9, 2, 8, 3, 6]);
