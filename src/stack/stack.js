f1a3ef2dd83a010ea238997735e6d6a071925b1f
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-14 21:05:25 +0530

    stack

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -3,6 +3,16 @@ class stack {
     this.items = [];
     this.top = null;
   }
+  getTop() {
+    if (this.items.length === 0) return null;
+    return this.top;
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
 }
 
 var myStack = new stack();
