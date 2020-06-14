7156ea9aac7ae32b1b4ca189b25f685e8b73ec5f
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-14 21:30:37 +0530

    stack

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -13,7 +13,33 @@ class stack {
   size() {
     return this.items.length;
   }
+  push(element) {
+    this.items.push(element);
+    this.top = element;
+  }
+  pop() {
+    if (this.items.length !== 0) {
+      if (this.items.length === 1) {
+        this.top = null;
+        return this.items.pop();
+      } else {
+        this.top = this.items[this.items.length - 2];
+        return this.items.pop();
+      }
+    } else return null;
+  }
 }
 
 var myStack = new stack();
 console.log("You have successfully created a Stack.");
+
+for (var i = 0; i < 5; i++) {
+  myStack.push(i);
+}
+console.log("Is stack empty? " + myStack.isEmpty());
+console.log("top: " + myStack.getTop());
+
+for (var i = 0; i < 5; i++) {
+  console.log("Element poped" + myStack.pop());
+  console.log("top :" + myStack.getTop());
+}
