8b478a84c4a54b6e76c3dcbf34b5e4bfcd5904b8
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-16 08:09:49 +0530

    reverse

--- src/practice.js
+++ src/practice.js
@@ -1,49 +1,104 @@
-class twostacks {
-  constructor(s) {
-    this.arr = [];
-    this.size = s;
-    this.top1 = -1;
-    this.top2 = s;
+class Stack {
+  constructor() {
+    this.items = [];
+    this.top = null;
   }
-  push1(value) {
-    if (this.top1 < this.top2 - 1) {
-      this.top1++;
-      this.arr[this.top1] = value;
-    }
+
+  getTop() {
+    if (this.items.length === 0) return null;
+    return this.top;
   }
-  push2(value) {
-    if (this.top1 < this.top2 - 1) {
-      this.top2--;
-      this.arr[this.top2] = value;
-    }
+
+  isEmpty() {
+    return this.items.length === 0;
   }
-  pop1() {
-    if (this.top1 >= 0) {
-      let value = this.arr[this.top1];
-      this.top1--;
-      return value;
-    } else {
-      return -1;
-    }
+
+  size() {
+    return this.items.length;
   }
-  pop2() {
-    if (this.top2 < this.size) {
-      let value = this.arr[this.top2];
-      this.top2++;
-      return value;
-    } else {
-      return -1;
-    }
+
+  push(element) {
+    this.items.push(element);
+    this.top = element;
   }
-  print() {
-    console.log(this.arr);
+
+  pop() {
+    if (this.items.length !== 0) {
+      if (this.items.length === 1) {
+        this.top = null;
+        return this.items.pop();
+      } else {
+        this.top = this.items[this.items.length - 2];
+        return this.items.pop();
+      }
+    } else return null;
   }
 }
 
-let mystack = new twostacks(3);
-mystack.push1(0);
-mystack.push2(2);
-mystack.push1(1);
-mystack.print(); // 0,1,2
-mystack.pop1();
-mystack.print();
+class Queue {
+  constructor() {
+    this.items = [];
+    this.front = null;
+    this.back = null;
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  getFront() {
+    if (this.items.length !== 0) {
+      return this.items[0];
+    } else return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.items.length === 0) {
+      return null;
+    } else {
+      return this.items.shift();
+    }
+  }
+}
+function reverseK(queue, k) {
+  if (queue.isEmpty() == false) {
+    let myStack = new Stack();
+    var count = 0;
+    while (count < k) {
+      myStack.push(queue.dequeue());
+      count++;
+    }
+
+    while (myStack.isEmpty() === false) {
+      queue.enqueue(myStack.pop());
+    }
+
+    for (var i = 0; i < queue.size() - k; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+  }
+
+  return queue;
+}
+var queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+queue.enqueue(8);
+queue.enqueue(9);
+queue.enqueue(10);
+//console.log(queue);
+reverseK(queue, 5);
+console.log(queue);
--- src/stack/README.md
+++ src/stack/README.md
@@ -385,3 +385,130 @@ All the operations take constant time because the array is being indexed and not
 </p>
 
 </details>
+
+<details><summary>Reversing First k Elements of Queue
+</summary>
+<p>
+
+```
+https://www.youtube.com/watch?v=VECGnvhkh-s
+```
+
+```
+
+```
+
+```javascript
+class Stack {
+  constructor() {
+    this.items = [];
+    this.top = null;
+  }
+
+  getTop() {
+    if (this.items.length === 0) return null;
+    return this.top;
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  push(element) {
+    this.items.push(element);
+    this.top = element;
+  }
+
+  pop() {
+    if (this.items.length !== 0) {
+      if (this.items.length === 1) {
+        this.top = null;
+        return this.items.pop();
+      } else {
+        this.top = this.items[this.items.length - 2];
+        return this.items.pop();
+      }
+    } else return null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.items = [];
+    this.front = null;
+    this.back = null;
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  getFront() {
+    if (this.items.length !== 0) {
+      return this.items[0];
+    } else return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.items.length === 0) {
+      return null;
+    } else {
+      return this.items.shift();
+    }
+  }
+}
+function reverseK(queue, k) {
+  if (queue.isEmpty() == false) {
+    let myStack = new Stack();
+    var count = 0;
+    while (count < k) {
+      myStack.push(queue.dequeue());
+      count++;
+    }
+
+    while (myStack.isEmpty() === false) {
+      queue.enqueue(myStack.pop());
+    }
+
+    for (var i = 0; i < queue.size() - k; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+  }
+
+  return queue;
+}
+var queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+queue.enqueue(8);
+queue.enqueue(9);
+queue.enqueue(10);
+//console.log(queue);
+reverseK(queue, 5);
+console.log(queue);
+```
+
+```
+ Time complexity is O(n) as all nn elements have to be processed with constant-time​ operations
+```
+
+</p>
+
+</details>
