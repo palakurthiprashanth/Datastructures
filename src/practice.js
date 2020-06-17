e583dc802cd16f61ad173103795ae3c2349fc481
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-17 22:41:08 +0530

    graph

--- src/practice.js
+++ src/practice.js
@@ -1,71 +1,198 @@
-class Stack {
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.nextElement = null;
+  }
+}
+class LinkedList {
   constructor() {
-    this.items = [];
-    this.top = null;
+    this.head = null;
   }
 
-  getTop() {
-    if (this.items.length == 0) return null;
-    return this.top;
+  //Insertion At Head
+  insertAtHead(newData) {
+    let tempNode = new Node(newData);
+    tempNode.nextElement = this.head;
+    this.head = tempNode;
+    return this; //returning the updated list
   }
 
   isEmpty() {
-    return this.items.length == 0;
+    return this.head == null;
   }
 
-  size() {
-    return this.items.length;
-  }
-
-  push(element) {
-    this.items.push(element);
-    this.top = element;
-  }
-
-  pop() {
-    if (this.items.length != 0) {
-      if (this.items.length == 1) {
-        this.top = null;
-        return this.items.pop();
-      } else {
-        this.top = this.items[this.items.length - 2];
-        return this.items.pop();
-      }
-    } else return null;
-  }
-}
-class minStack {
-  constructor() {
-    this.minimumstack = new Stack();
-    this.mainstack = new Stack();
-  }
-  pop() {
-    this.minimumstack.pop();
-    return this.mainstack.pop();
-  }
-  push(value) {
-    this.mainstack.push(value);
-    if (this.minimumstack.isEmpty()) {
-      this.minimumstack.push(value);
-      return;
-    }
-    if (value < this.minimumstack.getTop()) {
-      this.minimumstack.push(value);
+  //function to print the linked list
+  printList() {
+    if (this.isEmpty()) {
+      console.log("Empty List");
+      return false;
     } else {
-      this.minimumstack.push(this.minimumstack.getTop());
+      let temp = this.head;
+      while (temp != null) {
+        process.stdout.write(String(temp.data));
+        process.stdout.write(" -> ");
+        temp = temp.nextElement;
+      }
+      console.log("null");
+      return true;
     }
   }
-  min() {
-    return this.minimumstack.getTop();
+
+  getHead() {
+    return this.head;
+  }
+  setHead(newHead) {
+    this.head = newHead;
+    return this;
+  }
+  getListStr() {
+    if (this.isEmpty()) {
+      console.log("Empty List");
+      return "null";
+    } else {
+      let st = "";
+      let temp = this.head;
+      while (temp != null) {
+        st += String(temp.data);
+        st += " -> ";
+        temp = temp.nextElement;
+      }
+      st += "null";
+      return st;
+    }
+  }
+  insertAtTail(newData) {
+    //Creating a new Node with data as newData
+    let node = new Node(newData);
+
+    //check for case when list is empty
+    if (this.isEmpty()) {
+      //Needs to Insert the new node at Head
+      this.head = node;
+      return this;
+    }
+
+    //Start from head
+    let currentNode = this.head;
+
+    //Iterate to the last element
+    while (currentNode.nextElement != null) {
+      currentNode = currentNode.nextElement;
+    }
+
+    //Make new node the nextElement of last node of list
+    currentNode.nextElement = node;
+    return this;
+  }
+  search(value) {
+    //Start from the first element
+    let currentNode = this.head;
+
+    //Traverse the list until you find the value or reach the end
+    while (currentNode != null) {
+      if (currentNode.data === value) {
+        return true; //value found
+      }
+      currentNode = currentNode.nextElement;
+    }
+    return false; //value not found
+  }
+  deleteAtHead() {
+    //if list is empty, do nothing
+    if (this.isEmpty()) {
+      return this;
+    }
+    //Get the head and first element of the list
+    let firstElement = this.head;
+
+    //If list is not empty, link head to the nextElement of firstElement
+    this.head = firstElement.nextElement;
+
+    return this;
+  }
+  deleteVal(value) {
+    let deleted = null; //True or False
+    //Write code here
+
+    //if list is empty return false
+    if (this.isEmpty()) {
+      return false;
+    }
+
+    //else get pointer to head
+    let currentNode = this.head;
+    // if first node's is the node to be deleted, delete it and return true
+    if (currentNode.data == value) {
+      this.head = currentNode.nextElement;
+      return true;
+    }
+
+    // else traverse the list
+    while (currentNode.nextElement != null) {
+      // if a node whose next node has the value as data, is found, delete it from the list and return true
+      if (currentNode.nextElement.data == value) {
+        currentNode.nextElement = currentNode.nextElement.nextElement;
+        return true;
+      }
+      currentNode = currentNode.nextElement;
+    }
+    //else node was not found, return false
+    deleted = false;
+    return deleted;
+  }
+  deleteAtTail() {
+    // check for the case when linked list is empty
+    if (this.isEmpty()) {
+      return this;
+    }
+    //if linked list is not empty, get the pointer to first node
+    let firstNode = this.head;
+    //check for the corner case when linked list has only one element
+    if (firstNode.nextElement == null) {
+      this.deleteAtHead();
+      return this;
+    }
+    //otherwise traverse to reach second last node
+    while (firstNode.nextElement.nextElement != null) {
+      firstNode = firstNode.nextElement;
+    }
+    //since you have reached second last node, just update its nextElement pointer to point at null, skipping the last node
+    firstNode.nextElement = null;
+    return this;
   }
 }
 
-var stack = new minStack();
-stack.push(5);
-stack.push(2);
-stack.push(4);
-stack.push(1);
-stack.push(3);
-stack.push(9);
-
-console.log("minimum value: ", stack.min());
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.list = [];
+    for (var i = 0; i < vertices; i++) {
+      let temp = new LinkedList();
+      this.list.push(temp);
+    }
+  }
+  printGraph() {
+    console.log(">>Adjacency List of Directed Graph<<");
+    var i;
+    for (i = 0; i < this.list.length; i++) {
+      console.log("|" + String(i) + "| => ");
+      let temp = this.list[i].getHead();
+      while (temp != null) {
+        console.log("[" + String(temp.data) + "] -> ");
+        temp = temp.nextElement;
+      }
+      console.log("null ");
+    }
+  }
+  addEdge(source, destination) {
+    if (source < this.vertices && destination < this.vertices) {
+      this.list[source].insertAtHead(destination);
+    }
+  }
+}
+let g = new Graph(4);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 3);
+g.printGraph();
